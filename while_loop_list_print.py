446d3765eb4cd9a899a1c4e81d76e136b0b5dca1
Tamal H <tamalnh@users.noreply.github.com>
2016-12-08 21:01:14 +0600

    Add files via upload

---
+++ while_loop_list_print.py
@@ -0,0 +1,9 @@
+languages = ["PYTHON","PHP", "JAVA", "C++", "SWIFT", "GO", "C#", "JAVASCRIPT"]
+
+start_index = 0
+max_index = len(languages) - 1
+
+while start_index <= max_index:
+    language = languages[start_index]
+    print(language + " Is Awesome!")
+    start_index = start_index + 1
