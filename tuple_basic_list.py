e6b251187bf826b63facceee374f1dc504df5f6a
Tamal H <tamalnh@users.noreply.github.com>
2016-12-09 22:03:07 +0600

    Add files via upload

---
+++ tuple_basic_list.py
@@ -0,0 +1,3 @@
+obj_list = (("Admin", "Oparetor", "User"), ("Coding", "Programming"), [2, 4, 6], {"wannabe":"programmer"})
+
+print(obj_list[3]["wannabe"])#Value count start with ZERO(0) like an ARRAY
