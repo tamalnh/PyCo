e82aae2d15f07671183ff13122e3ef969abdffd5
Tamal H <tamalnh@users.noreply.github.com>
2016-12-10 18:12:07 +0600

    Update 2d_array_convert_1d_array.py

--- 2d_array_convert_1d_array.py
+++ 2d_array_convert_1d_array.py
@@ -4,11 +4,11 @@ matrix_2d = [[1, 2, 3], [4, 5, 6]]#is a 2D array
 for row in matrix_2d:
     for num in row:
         matrix_1d.append(num)
-print(matrix_1d) # 2 array will convert into one variable
+print(matrix_1d) #2 array will convert into one variable
 
 print("-------------------------------")
 print("By using list comprehension")
-######### By using comprehension
+#By using comprehension
 
 matrix_1d = [num for row in matrix_2d for num in row]
 print(matrix_1d)
