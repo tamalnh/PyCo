e34bc8890ccdfce65f36040a3cc90b66f89fa275
Tamal H <tamalnh@users.noreply.github.com>
2016-12-11 20:04:15 +0600

    Update multiple_peramitar_handeling_in_a_function.py

--- multiple_peramitar_handeling_in_a_function.py
+++ multiple_peramitar_handeling_in_a_function.py
@@ -4,3 +4,17 @@ def multi_agrs(*args):
         sum += num
     return sum
 print(multi_agrs(20, 30, 40, 10))
+
+#**************************#
+def p_dict(**args):
+    for arg in args:
+        print("{0}:{1}".format(arg, args[arg]))
+p_dict(a=2, b=3, c=4)
+    
+#**************************#
+
+def all_print(a, *args, **largs):
+    print(a)
+    print(args)
+    print(largs)
+all_print(10, 20, 30, c=3, b=4, d=5)      
